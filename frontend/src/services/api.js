3f9a0adda4209db2326ede919d1acb7e095ec373
Isha Paliwal <ishasona97@gmail.com>
2025-07-11 17:21:42 -0400

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team7finalproject-production.up.railway.app/api",
+  baseURL: "https://team7finalproject-production-46be.up.railway.app",
   // baseURL: "http://localhost:5000/api",
 });
 
